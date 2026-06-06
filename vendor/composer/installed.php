09e6dd45a729e1cefca2c3ec2427e038ec8692c2
Puleeno Nguyen <puleeno@gmail.com>
2026-06-06 19:34:19 +0700

    chore: initialize composer autoloader for google-maps-embedding extension

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'puleeno/jankx-google-maps-embedding',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'b336cdc8b78f00b99b551a673b491ab155a8cd0d',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'puleeno/jankx-google-maps-embedding' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'b336cdc8b78f00b99b551a673b491ab155a8cd0d',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
